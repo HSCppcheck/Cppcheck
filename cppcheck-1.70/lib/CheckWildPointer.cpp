29bca87417c5eaed0012f97000b0c4b9469fac66
ricenoddle <1953547787@qq.com>
2016-04-18 16:32:56 +0800

    开发规则的h和cpp文件

---
+++ cppcheck-1.70/lib/CheckWildPointer.cpp
@@ -0,0 +1,33 @@
+//---------------------------------------------------------------------------
+#include "checkwildpointer.h"
+#include "mathlib.h"
+#include "symboldatabase.h"
+#include <cctype>
+//---------------------------------------------------------------------------
+
+// Register this check class (by creating a static instance of it)
+namespace {
+	CheckWildPointer instance;
+}
+
+void CheckWildPointer::wildPointer()
+{
+
+}
+
+void CheckWildPointer::judgeDelete()
+{
+
+}
+
+void CheckWildPointer::wildPointerError(const Token *tok, const std::string strValue)
+{
+
+}
+
+void CheckWildPointer::judgeDeleteError(const Token *tok, const std::string strValue)
+{
+
+}
+//---------------------------------------------------------------------------
+
